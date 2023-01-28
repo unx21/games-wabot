9ec1b474af9ee8a9958924160d4d22f5d1b84f9e
unxzx <84166927+unx21@users.noreply.github.com>
2023-01-28 19:37:53 +0800

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,13 +1,15 @@
 let handler = async(m, { text }) => {
+    if (!isOwner) throw false
     let user = global.db.data.users[m.sender]
     user.afk = +new Date
     user.afkReason = text
-    m.reply(`
+    /* m.reply(`
 ${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
-`)
+`) */
 }
-handler.help = ['afk [alasan]']
-handler.tags = ['main']
-handler.command = /^afk$/i
+//handler.help = ['oafk [alasan]']
+//handler.tags = ['owner']
+handler.command = /^oafk$/i
+handler.register = true
 
 module.exports = handler
